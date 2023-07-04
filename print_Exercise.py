45fd8a13bd028e8a4e39263eec0d85eb58753239
JEREMIAH JACOB <reginajacobenam@gmail.com>
2023-07-04 02:16:20 +0100

    using print in python

---
+++ print_Exercise.py
@@ -0,0 +1,7 @@
+# Interactive coding exercise
+# Using the Print function
+# the print()fun outputs the user commands
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
