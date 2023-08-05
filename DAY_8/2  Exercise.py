bed0738fb7dcd2dc4361cf224b632436f4897b36
JEREMIAH JACOB <reginajacobenam@gmail.com>
2023-08-05 13:57:14 +0100

    Prime checker coding

--- DAY_8/2  Exercise.py
+++ DAY_8/2  Exercise.py
@@ -8,9 +8,9 @@ def prime_checker(number):
     for i in range(2, number -1):
         if number % i == 0:
             is_prime = False
-        if is_prime:
+    if is_prime:
             print("It's a prime number.")
-        else:
+    else:
             print("It's not a prime number.")
 
 
